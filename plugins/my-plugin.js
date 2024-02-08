83b6dab93bcdec36899120b33ccc16a18fafcb65
Elad Karni <elad.karni@gmail.com>
2024-02-08 09:26:48 -0500

    Working ManifestModifation and plugin atr input

--- plugins/my-plugin.js
+++ plugins/my-plugin.js
@@ -1,5 +1,22 @@
-const withMySDK = (config) => {
-  return config;
+const { AndroidConfig, withAndroidManifest } = require("expo/config-plugins");
+
+function addAttributesToApplication(androidManifest, attributes) {
+  const app = AndroidConfig.Manifest.getMainApplicationOrThrow(androidManifest);
+  if (app?.$) {
+    app.$ = { ...app.$, ...attributes };
+  }
+
+  return androidManifest;
+}
+
+const withMySDK = (expoConfig, languages) => {
+  console.log(languages);
+  return withAndroidManifest(expoConfig, (config) => {
+    config.modResults = addAttributesToApplication(config.modResults, {
+      "android:localeConfig": "@xml/locales_config",
+    });
+    return config;
+  });
 };
 
 module.exports = withMySDK;
