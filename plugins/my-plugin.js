40f0d67a6377e851e182ed691fa7e11385eb028c
Elad Karni <elad.karni@gmail.com>
2024-02-08 19:15:32 -0500

    Test commit ignoring android folder

--- plugins/my-plugin.js
+++ plugins/my-plugin.js
@@ -1,4 +1,9 @@
-const { AndroidConfig, withAndroidManifest } = require("expo/config-plugins");
+const {
+  AndroidConfig,
+  withAndroidManifest,
+  XML,
+} = require("expo/config-plugins");
+const { mkdirSync } = require("fs");
 
 function addAttributesToApplication(androidManifest, attributes) {
   const app = AndroidConfig.Manifest.getMainApplicationOrThrow(androidManifest);
@@ -9,14 +14,38 @@ function addAttributesToApplication(androidManifest, attributes) {
   return androidManifest;
 }
 
-const withMySDK = (expoConfig, languages) => {
+function addLanguages(languages) {
+  return languages.map((lang) => ({ $: { "android:name": lang } }));
+}
+
+function generateXML({ lang }) {
+  return {
+    "locale-config": {
+      $: {
+        "xmlns:android": "http://schemas.android.com/apk/res/android",
+      },
+      locale: addLanguages(lang),
+    },
+  };
+}
+
+async function writeXMLFiles(langs) {
+  const obj = generateXML(langs);
+  const dir = "android/app/src/main/res/xml";
+
+  mkdirSync(dir, { recursive: true });
+  await XML.writeXMLAsync({ path: `${dir}/locales_config.xml`, xml: obj });
+}
+
+const withRNLocalizeSettings = (expoConfig, languages) => {
   console.log(languages);
   return withAndroidManifest(expoConfig, (config) => {
     config.modResults = addAttributesToApplication(config.modResults, {
       "android:localeConfig": "@xml/locales_config",
     });
+    writeXMLFiles(languages);
     return config;
   });
 };
 
-module.exports = withMySDK;
+module.exports = withRNLocalizeSettings;
