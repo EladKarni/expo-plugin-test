7583da6179d583fe5b9b1c069d3841d3be124ff1
Elad <elad.karni@gmail.com>
2024-09-18 13:59:47 -0400

    Chor: Updated packages and added native folders to .ignore

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getLanguage } from 'react-native-localization-settings';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{getLanguage()}</Text>
       <StatusBar style="auto" />
     </View>
   );
